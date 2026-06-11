b56d8db4e65577cd5d04b60a25942d464597f6a1
Alyn Rivera <alyn.ri.ab@gmail.com>
2026-06-10 20:18:22 -0600

    Diseño de vista de registro

---
+++ app/src/main/java/com/example/examen3/model/SignUpActivity.kt
@@ -0,0 +1,29 @@
+package com.example.tuapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        val etUsuario = findViewById<EditText>(R.id.etNuevoUsuario)
+        val etContrasena = findViewById<EditText>(R.id.etNuevaContrasena)
+        val btnRegistrarse = findViewById<Button>(R.id.btnRegistrarse)
+
+        btnRegistrarse.setOnClickListener {
+            val usuario = etUsuario.text.toString()
+            val contrasena = etContrasena.text.toString()
+
+            if (usuario.isNotEmpty() && contrasena.isNotEmpty()) {
+                Toast.makeText(this, "Usuario $usuario registrado correctamente", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Por favor ingresa todos los datos", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
