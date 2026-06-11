4ee4dfcfd24b0b7d3fd96ee33f11999322181286
Alyn Rivera <alyn.ri.ab@gmail.com>
2026-06-10 20:19:41 -0600

    Diseño de vista de registro

--- app/src/main/java/com/example/examen3/model/SignUpActivity.kt
+++ app/src/main/java/com/example/examen3/model/SignUpActivity.kt
@@ -5,6 +5,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.examen3.R
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
